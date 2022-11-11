1e0767330f29761dd7b9e8109bf0e95721eb440e
Богдан К <epam.2@mail.ru>
2022-11-11 12:11:55 +0400

    Change Program.cs in ConsoleApp2, create Option.cs

---
+++ ConsoleApp2/ConsoleApp2/Option.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Option
+{
+    internal int[] CreateArray() // метод возвращает массив случайного размера и заполняется случайными числами
+    {
+        int[] arr = new int[new Random().Next(1, 50)];        
+
+        for(int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = new Random().Next(-100, 101);
+        }
+
+        return arr;
+    }
+
+    internal int SummOf_odd(int [] arr) // метод возвращает сумму нечётных элементов массива
+    {
+        int summ = 0;
+        for(int i = 1; i < arr.Length; i += 2)
+        {
+            summ += arr[i]; 
+        }
+        return summ;
+    }
+}
--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// В программе создаётся одномерный массив, заполненный случайными числами.
+// есть метод, который находит сумму элементов, стоящих на нечётных позициях.
+using System;
 
 namespace ConsoleApp2
 {
@@ -6,7 +8,18 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Option newArr = new Option();
+            int[] arr = newArr.CreateArray();
+
+            Option summ = new Option();
+            int showSumm = summ.SummOf_odd(arr);
+
+            Console.WriteLine($"Сумма нечётных элементов равна: {showSumm}.");
         }
+
+
     }
+
+    
 }
