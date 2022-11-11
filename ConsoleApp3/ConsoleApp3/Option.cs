2edd3efe4c104d414ad82ffe4a4bec75738d8043
Богдан К <epam.2@mail.ru>
2022-11-11 14:28:40 +0400

    create project ConsoleApp3

---
+++ ConsoleApp3/ConsoleApp3/Option.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Option
+{
+	public int[] CreateArray()
+	{
+		int[] arr = new int[new Random().Next(1, 20)];
+
+		for(int i = 0; i < arr.Length; i++)
+        {
+			arr[i] = new Random().Next(1, 1000);
+        }
+		
+		return arr;
+	}
+
+	public int Result(int[] arr)
+    {
+		int min = arr[0], max = arr[0];
+
+		for(int i = 1; i < arr.Length; i++)
+        {
+			if (min > arr[i]) min = arr[i];
+			if (max < arr[i]) max = arr[i];
+        }
+
+		return max - min;
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа создаёт массив вещественных чисел и находит разницу между максимальным и минимальным элементов массива.
+using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выводим значение на экран");
+            Option option = new Option();
+            
+            int[] arr = option.CreateArray();
+            Console.WriteLine(option.Result(arr));
+        }
+    }
+}
